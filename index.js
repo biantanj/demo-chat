6e795c259d44be552d68721afcc8d02afca9965f
unknown <10a2tam@gmail.com>
2019-08-03 02:22:29 +0700

    Update them

--- index.js
+++ index.js
@@ -2,10 +2,22 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
+var arrUser = [];
+var countUser = 0;
+
 io.on('connection', function(socket){
-  console.log('a user connected');
+  socket.on('ketNoi', function(userN){
+     var user = {id:socket.id,name:userN};
+     arrUser.push(user);
+     countUser++;
+     io.emit('notiKetNoi', userN);
+     io.emit('demNguoi', countUser);
+  });
   socket.on('disconnect', function(){
-    console.log('user disconnected');
+    
+    io.emit('notiMatKetNoi', arrUser.find(x=> x.id == socket.id).name);
+    countUser--;
+    io.emit('demNguoi', countUser);
   });
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
